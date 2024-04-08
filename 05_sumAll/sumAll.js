fb02dec2c137b59540f6a6a6deed6ca3df72c8e7
jeroen van Heiningen <theoprog95@gmail.com>
2024-04-08 20:47:46 +0200

    exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  if (typeof x !== "number" || typeof y !== "number" || x < 0 || y < 0)
+    return "ERROR";
+  fromInt = Math.min(x, y);
+  toInt = Math.max(x, y);
+  sum = 0;
+  for (let i = fromInt; i <= toInt; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
