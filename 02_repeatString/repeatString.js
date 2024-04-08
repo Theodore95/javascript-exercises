30210d04ab7a46aca48a5e2fa1c883e9a2a0fbf8
jeroen van Heiningen <theoprog95@gmail.com>
2024-04-08 20:04:24 +0200

    First three exercises completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeat) {
+  let stringBuilder = "";
+  if (repeat < 0) return "ERROR";
+  for (let i = 0; i < repeat; i++) {
+    stringBuilder += str;
+  }
+  return stringBuilder;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+// const reverseString = function (str) {
+//   let index = str.length - 1;
+//   let stringBuilder = "";
 
+//   for (index; index >= 0; index--) {
+//     stringBuilder += str[index];
+//   }
+
+//   return stringBuilder;
+// };
+
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
